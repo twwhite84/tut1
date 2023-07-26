a8027b03d95126ff27dca5efa2fc99cc7b10a9c7
twwhite84 <twwhite.pvt@gmail.com>
2023-07-26 20:37:57 +1200

    Added feature 3

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", " + name.toUpperCase();
+    return messages.getString("Greeting") + ", " + name.toUpperCase();
     //deleted markers and chose to keep .toUpperCase() version
   }
 
